b7ba65c5ab1fd62d99f57973707acde133a49488
Ville Uusitalo <75932298+Nyyhaus@users.noreply.github.com>
2021-04-10 08:50:48 +0300

    Create every_other_letter.py

---
+++ every_other_letter.py
@@ -0,0 +1,9 @@
+def every_other_letter(word):
+  return word[::2]
+# Uncomment these function calls to test your function:
+print(every_other_letter("Codecademy"))
+# should print Cdcdm
+print(every_other_letter("Hello world!"))
+# should print Hlowrd
+print(every_other_letter(""))
+# should print 
