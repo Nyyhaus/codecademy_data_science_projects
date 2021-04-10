213f52d23c7bb6eef833600b09ea6788952f586e
Ville Uusitalo <75932298+Nyyhaus@users.noreply.github.com>
2021-04-10 08:35:17 +0300

    Create check_for_name.py

---
+++ check_for_name.py
@@ -0,0 +1,13 @@
+def check_for_name(sentence, name):
+  if name.lower() in sentence.lower():
+    return True
+  else:
+    return False
+
+# Uncomment these function calls to test your  function:
+print(check_for_name("My name is Jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is Samantha", "Jamie"))
+# should print False
